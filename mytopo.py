aaf89cdae4c5325dc1623a04fc26bf9ba6ae2e5f
sysong13 <34019939+sysong13@users.noreply.github.com>
2018-07-11 15:55:53 +0800

    Add files via upload

---
+++ mytopo.py
@@ -0,0 +1,30 @@
+from mininet.topo import Topo
+
+class MyTopo(Topo):
+    def __init__(self):
+        Topo._init_(self)
+
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
+
+        s1=self.addSwitch('s1')
+        s2=self.addSwitch('s2')
+        s3=self.addSwitch('s3')
+        s4=self.addSwitch('s4')
+        s5=self.addSwitch('s5')
+
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s1)
+        self.addLink(h4, s2)
+        self.addLink(h5, s2)
+        self.addLink(h6, s2)
+
+topos ={'mytopo':(lambda :MyTopo)}
+
+
